055c55efbf3092a93f3b95061b7e798a775b5812
Iaroslav Lomonos <iaroslav.lomonos@zalando.de>
2018-02-20 16:28:29 +0100

    Iterative tasks

---
+++ src/main/java/task1/Iterative.java
@@ -0,0 +1,25 @@
+package task1;
+
+/*
+Для заданного диапазона натуральных чисел вычислить количество чисел, удовлетворяющих условию:
+- сумма цифр числа равна заданной. Пример: для заданной суммы 12, число 462 удовлетворяет условию
+- сумма цифр в первой половине числа равна сумме цифр во второй половине числа. Пример: 351432, 8796
+- число делится на заданное число без остатка
+ */
+class Iterative {
+
+    public Iterative(final Long begin, final Long end) {
+    }
+
+    long amountOfNubersWithDigitSumEqualsTo(final long sum) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    long amountOfLuckyNumbers() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    long amountOfMultipleTo(final long multiplier) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
